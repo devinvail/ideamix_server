c57e35820dfc9c331aece24cf6d8bb0337fd6b13
Devin Vail <devinvail@Devins-MacBook-Pro-2109.local>
2017-10-30 13:09:53 -0400

    updated server

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+  // Initialize SuperLogin 
+var superlogin = new SuperLogin(config);
+
+// Mount SuperLogin's routes to our app 
+app.use('/auth', superlogin.router);
+
+app.listen(process.env.PORT || 8080);
+
+
 var express = require('express');
 var https = require('https');
 var bodyParser = require('body-parser');
@@ -14,9 +23,9 @@ app.use(cors());
 var config = {
   dbServer: {
     protocol: 'https://',
-    host: '[YOUR-HOST]',
-    user: '[YOUR-USER]',
-    password: '[YOUR-PASSWORD]',
+    host: 'ce4d2d12-3865-486c-86a1-3dd2692dba59-bluemix.cloudant.com',
+    user: 'ce4d2d12-3865-486c-86a1-3dd2692dba59-bluemix',
+    password: '85e0948009f9a637d5c235571fd9be2b032b4d9e23eda155b8110c0b7ea6d926',
     cloudant: true,
     userDB: 'hangz-users',
     couchAuthDB: '_users'
